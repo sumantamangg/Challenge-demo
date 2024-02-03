a139486941fdecea003c0fa4b5364ec4642731b9
Suman Tamang <sumantamang1020@gmail.com>
2024-02-03 10:50:10 +1030

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { General } from '../assets/colors.data';
+import { Primary } from '../assets/colors.data';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatGridListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Mockup-App';
+  general:any;
+  primary:any;
+
+  ngOnInit(){
+    this.general = General;
+    this.primary = Primary;
+  }
 }
---
+++ src/assets/colors.data.ts
@@ -0,0 +1,91 @@
+export const General ={
+    name: "General",
+    value: [
+        {
+            name: "General 100",
+            value: "#131523",
+            textColor: "white"
+        },
+        {
+            name: "General 90",
+            value: "#333752",
+            textColor: "white"
+        },
+        {
+            name: "General 100",
+            value: "#5A607F",
+            textColor: "white"
+        },
+        {
+            name: "General 70",
+            value: "#7E84A3",
+            textColor: "white"
+        },
+        {
+            name: "General 80",
+            value: "#A1A7C4",
+            textColor: "black"
+        },
+        {
+            name: "General 50",
+            value: "#D7DBEC",
+            textColor: "black"
+        },
+        {
+            name: "General 40",
+            value: "#E6E9F4",
+            textColor: "black"
+        },
+        {
+            name: "General 30",
+            value: "#F5F6FA",
+            textColor: "black"
+        }
+    ]
+}
+
+export const Primary ={
+    name: "Primary",
+    value: [
+        {
+            name: "Primary 100",
+            value: "#1E5EFF",
+            textColor: "white"
+        },
+        {
+            name: "Primary 90",
+            value: "#336DFF",
+            textColor: "white"
+        },
+        {
+            name: "Primary 80",
+            value: "#4F81FF",
+            textColor: "white"
+        },
+        {
+            name: "Primary 70",
+            value: "#608DFF",
+            textColor: "white"
+        },
+        {
+            name: "Primary 60",
+            value: "#89ABFF",
+            textColor: "black"
+        },
+        {
+            name: "Primary 50",
+            value: "#B6CBFF",
+            textColor: "black"
+        },
+        {
+            name: "Primary 40",
+            value: "#D9E4FF",
+            textColor: "black"
+        },
+        {
+            name: "Primary 30",
+            value: "#ECF2FF",
+            textColor: "black"
+        }
+    ]
+}
